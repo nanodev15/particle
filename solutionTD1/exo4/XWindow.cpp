33f8c5585eb363debaa7f7cc6481b13f580dc836
nanodev15 <60925394+nanodev15@users.noreply.github.com>
2024-05-13 17:10:21 -0400

    Update XWindow.cpp

--- solutionTD1/exo4/XWindow.cpp
+++ solutionTD1/exo4/XWindow.cpp
@@ -1,13 +1,3 @@
-/**
- Author: David Auber
- Email : auber@labri.fr
- Last modification : 01/09/2004
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by  
- the Free Software Foundation; either version 2 of the License, or     
- (at your option) any later version.
-*/
-
 #include <cstdlib>
 #include <iostream>
 #include "XWindow.h"
